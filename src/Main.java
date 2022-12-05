62056f40daf6bfeca1fc1e9039134651f1eb681d
Verkin78 <otrajenie.jc@gmail.com>
2022-12-05 23:54:16 +0300

    OOP Люди

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        Human human1 = new Human("Максим", "Минск", 1988,
+                "бренд-менеджер(ом).");
+        Human human2 = new Human("Аня", "Москва", 1993,
+                "методист(ом) образовательных программ.");
+        Human human3 = new Human("Катя", "Калининград", 1992,
+                "продакт-менеджер(ом).");
+        Human human4 = new Human("Артём", "Москва", 1995,
+                "директор(ом) по развитию бизнеса.");
+        Human human5 = new Human(null, null, -1234, null);
 
-        Person person1 = new Person("Максим", "Минск", 1988,
-        Person person2 = new Person("Аня", "Москва", 1993,
-        Person person3 = new Person("Катя", "Калининград", 1992,
-        Person person4 = new Person("Артём", "Москва", 1995,
-        Person person5 = new Person(null, null, -1234, null);
 
-        System.out.println(person1);
-        System.out.println(person2);
-        System.out.println(person3);
-        System.out.println(person4);
-        System.out.println(person5);
+
+        System.out.println(human1);
+        System.out.println(human2);
+        System.out.println(human3);
+        System.out.println(human4);
+        System.out.println(human5);
+
+
     }
 }
