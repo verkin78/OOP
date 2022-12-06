2e0701a0c6bf0287d65b8d4514fc034cb5cbe24c
Verkin78 <otrajenie.jc@gmail.com>
2022-12-06 16:14:27 +0300

    OOP Цветы

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        Person person1 = new Person("Максим", "Минск", 1988,
-        Person person2 = new Person("Аня", "Москва", 1993,
-        Person person3 = new Person("Катя", "Калининград", 1992,
-        Person person4 = new Person("Артём", "Москва", 1995,
-        Person person5 = new Person(null, null, -1234, null);
+       Flower flower1 = new Flower("Роза обыкновенная", "Голландия", "35.59", "");
+        Flower flower2 = new Flower("Хризантема", "", "15", "5");
+        Flower flower3 = new Flower("Пион", "Англия", "69.9", "1");
+        Flower flower4 = new Flower("Гипсофила", "Турция", "19.5", "10");
+        Flower flower5 = new Flower(null, null, null, null);
 
-        System.out.println(person1);
-        System.out.println(person2);
-        System.out.println(person3);
-        System.out.println(person4);
-        System.out.println(person5);
+        System.out.println(flower1);
+        System.out.println(flower2);
+        System.out.println(flower3);
+        System.out.println(flower4);
+        System.out.println(flower5);
     }
 }
